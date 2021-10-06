9f1908b6483d47ebe287d84ce9a919392eb26184
Moreira Rubén Darío <moreirard@gmail.com>
2021-10-05 22:12:11 -0300

    Se corrige funcionalidad al cerrar cerradura

--- src/cerraduraElectronica/CerraduraElectronica.java
+++ src/cerraduraElectronica/CerraduraElectronica.java
@@ -3,8 +3,14 @@ package cerraduraElectronica;
 public class CerraduraElectronica {
 
 	private Boolean abierta = true;
+
 	public boolean estaAbierta() {
 		return abierta;
+	}
+
+	public void cerrar() {
+		this.abierta = false;
+
 	}
 
 }
